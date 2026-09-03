d9bacc5a383e24906d9d254eff869d7deb9751a7
Romanch Kolhe <officialromanch04@gmail.com>
2026-09-03 09:26:44 +0530

    Time: 9 ms (34.78%) | Memory: 122 MB (8.70%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        boolean hasOdd = false;
+
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                hasOdd = true;
+                break;
+            }
+        }
+
+        if (!hasOdd) {
+            return true;
+        }
+
+        int minOdd = Integer.MAX_VALUE;
+
+        for (int num : nums1) {
+            if (num % 2 != 0) {
+                minOdd = Math.min(minOdd, num);
+            }
+        }
+
+        for (int num : nums1) {
+            if (num % 2 == 0 && num < minOdd) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
