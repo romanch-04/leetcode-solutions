2b0e984e108c393bd570fd62667c5508ecbc9d74
Romanch Kolhe <officialromanch04@gmail.com>
2026-09-06 20:01:16 +0530

    Time: 456 ms (51.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,19 @@
+/* Write your PL/SQL query statement below */
+SELECT id, num
+FROM (
+    SELECT
+        id,
+        COUNT(*) AS num
+    FROM (
+        SELECT requester_id AS id
+        FROM RequestAccepted
+
+        UNION ALL
+
+        SELECT accepter_id AS id
+        FROM RequestAccepted
+    )
+    GROUP BY id
+    ORDER BY num DESC
+)
+WHERE ROWNUM = 1;
